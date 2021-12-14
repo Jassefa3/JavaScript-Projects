269da594a28ca819daedc2581afb5c21dcbe3820
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-14 11:38:21 -0600

    java

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+window.alert("Hello! I am an alert box!!");
+
+document.write('Hello, world!');
+
+var A = "This is a string";
+
+document.write(A);
+
+var B = "Second string to";
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+
+window.alert(B);
+
+document.write("\"Be who you are and say what you feel,"
+            +" because those who mind don\'t matter and those who matter don\'t mind.\""
+            +"-Dr. Seuss");
+
+            var B = "Concatenated" + " String"
+            document.write(B)
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue"); //making color blue
+document.write(blues);   
+
+
+{p
+Document.write(3+3);
+}
