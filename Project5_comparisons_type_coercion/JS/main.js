35c35fe9124e8e227c475a80f63e8d2923ab87b4
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-15 13:17:47 -0600

    5

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -12,7 +12,14 @@ console.log(3+3);
 
 
 document.write(((points == 4) || (score == 4)));
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
 function const_function() {
+    document.getElementById("or").innerHTML = (5 > 10 || 10 > 4);
+    document.getElementById("and").innerHTML = (5 > 2 && 10 > 4);
     document.getElementById("Test").innerHTML = isNaN('This is a string');
     document.getElementById('Test2').innerHTML = isNaN('007');
     document.getElementById("datavalue").innerHTML = (10 === 10);
