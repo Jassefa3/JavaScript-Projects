d195de5d4c6ba43b78b4e084a86b55b8f7bef7c8
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-15 09:28:45 -0600

    4

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,29 @@
+let car = "Fiat";
+
+const car = {type:"Fiat", model:"500", color:"white"};
+
+objectName.propertyName
+
+const person = const_function() {       //example of method function
+    firstName: "Dictionary",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() 
+      return this.firstName + " " + this.lastName;
+     }
+
+  x = new string();        // Declares x as a String object
+y = new number();        // Declares y as a Number object
+z = new boolean();       // Declares z as a Boolean object
+
+function my_Dictionary() {
+    var Animal = {
+        species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
