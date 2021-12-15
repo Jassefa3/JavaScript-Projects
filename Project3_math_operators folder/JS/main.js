ab701a1d69f87d623cd1ee9e820c08187e27d776
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-15 08:39:08 -0600

    project

---
+++ Project3_math_operators folder/JS/main.js
@@ -0,0 +1,47 @@
+document.write(10+3);
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Math").innerHTML = result;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5-2;
+    document.getElementById("Subtract").innerHTML = "5 - 2 = "+ Subtraction;
+}
+
+function multipication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Multiply").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48/6;
+    document.getElementById("Divide").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Multiple").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math; 
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("modulus").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("negation").innerHTML = -x;
+}
+
+var X = 5;
+X++;
+document.write(X);
+
+var X = 5.25;
+X--;
+document.write(X);
+
+window.alert(Math.random());
