25e616323f142c31c767596e8e2452c3c1acfbee
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-16 10:35:12 -0600

    8

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,45 @@
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function func() {
+	var str = 'geeksforgeeks';
+	var string = str.toUpperCase();
+	document.getElementById("string").innerHTML = Section;
+}
+
+let text = "Mr. Blue has a blue house";
+let position = text.search("Blue");
+
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+
+function fixed_Method() {
+    var totn_string = 'TechOnTheNet';
+    var string = string.fixed()
+    document.getElementById("Fixed").innerHTML = X.tostring();
+}
+
+function valueOf_Method() {
+    var totn_string = 'the best';
+    var string = string.valueOf()
+    document.getElementById("value").innerHTML = X.tostring();
+}
