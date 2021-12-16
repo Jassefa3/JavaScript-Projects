2eb5eac39a6686a23271994922713ef25981d54b
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-16 13:15:50 -0600

    1

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,41 +1,7 @@
-var X = 10;
-function Add_numbers_1() {
-    document.write(20 + X + "<br>");
-}
-function Add_numbers_2() {
-    document.write(X + 100);
-}
-Add_numbers_1();
-Add_numbers_2();
-
-function Add_numbers_1() {
-    var X = 10;
-    document.write(20 + X + "<br>");
-}
-function Add_numbers_2() {
-    document.write(X + 100);
-}
-Add_numbers_1();
-Add_numbers_2();
-
-if (1<2) {
-    document.write("The left number is smaller than the number on the right.")
-}
-
-function get_Date() {
-    if (new date().getHours() < 18) {
-        document.getElementById("Greeting").innerHTML = "How are you today?";
-    }
-}
-
-function Big_person() {
-    if (new)
-}
-
 function Age_Function() {
     Age = document.getElementById("Age").value;
     if (Age >= 18) {
-        Vote = "You are not old enough to vote!";
+        Vote = "You are old enough to vote!";
     }
     else {
         Vote = "You are not old enough to vote!";
