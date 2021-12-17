edd12deec5b97e811b685f954d539a38d2d8fcca
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-16 21:44:01 -0600

    yes

---
+++ Project10_loops_arrays/JS/main.js
@@ -0,0 +1,46 @@
+function string_Method() {
+let text = "Hello World!";
+let length = text.length;
+document.getElementById("demo").innerHTML = length;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+  function array_Function() {
+      var Custom_Picture = [];
+      Custom_Picture[0] = "sleeping";
+      Custom_Picture[1] = "playing";
+      Custom_Picture[2] = "eating";
+      Custom_Picture[3] = "purring";
+      document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[2] + ".";
+  }
+
+  function constant_function() {
+      const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+      Musical_Instrument.color = "blue";
+      Musical_Instrument.price = "$900";
+      document.getElementById("Constant").innerHTML = "The cost of the " +
+        Musical_Instrument.type + "was" + Musical_Instrument.price;
+  }
+
+  let car = {
+      make: "Dodge ",
+      model: "Viper ",
+      year: "2021 ",
+      color: "red ",
+      description : function() {
+          return "The car is a " + this.year + this.color + this.make + this.model;
+      }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+  
