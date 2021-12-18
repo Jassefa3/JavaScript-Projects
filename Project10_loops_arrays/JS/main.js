15d384ffe4c61889a3af6c617785932cbba937c6
Jassefa3 <91398753+Jassefa3@users.noreply.github.com>
2021-12-17 19:06:37 -0600

    done

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -40,7 +40,8 @@ function for_Loop() {
       description : function() {
           return "The car is a " + this.year + this.color + this.make + this.model;
       }
+      document.getElementById("Car_Object").innerHTML = car.description();
 };
-document.getElementById("Car_Object").innerHTML = car.description();
+
 
   
